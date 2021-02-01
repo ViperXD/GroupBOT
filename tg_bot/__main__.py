a24afeea9746446844fe0788d763f7e35cd054f1
Good Boy <78008481+v-v-r-official@users.noreply.github.com>
2021-02-01 07:13:33 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -370,7 +370,7 @@ def donate(bot: Bot, update: Update):
         update.effective_message.reply_text(DONATE_STRING, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
 
         if OWNER_ID != 254318997 and DONATION_LINK:
-            update.effective_message.reply_text("Join On My Updates Channel "
+            update.effective_message.reply_text(" You can also donate to the person currently running me "
                                                 "[HERE]({})".format(DONATION_LINK),
                                                 parse_mode=ParseMode.MARKDOWN)
 
