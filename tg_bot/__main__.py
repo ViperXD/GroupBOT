1f40b3445c21841be8f6e7bf48970b7383fab886
Good Boy <78008481+v-v-r-official@users.noreply.github.com>
2021-02-01 07:04:28 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,11 +18,18 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-hoi {}, my name is {}!  I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.
+hoi {}, my name is {}! if you have any questions about how to use me please give me /help... 
 
-My future updates will be put into This Channel - @vkprojects & My Support Group @vkpbots.
+im a group manager bot maintained by  [this person](tg://user?id={}).
 
-For more commands click /help ,
+My future updates will be put into This Channel - @Vkprojects & My Support Group @Vkpbots.
+
+This is my [Deploy Code](https://heroku.com/deploy?template=https://github.com/v-v-r-official/GroupBOT),
+you can create clone same like me..
+
+For more commands click /help...
+
+**Keep in mind that any changes you DO do to the source have to be on github, as per the license.**
 """
 
 HELP_STRINGS = """
